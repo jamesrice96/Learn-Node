599cc2e375ed33ea4273ab2fb937fa51a5bb0d3a
James Rice <james@purplecs.com>
2018-09-26 16:53:29 +0100

    creating the add / edit store form and controller methods

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,8 +1,11 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'james';
-  next();
-};
-
 exports.homePage = (req, res) => {
   res.render('index');
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
 };
