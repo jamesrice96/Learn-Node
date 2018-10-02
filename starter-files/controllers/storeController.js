dcd2c844b1efe081dcf7149d57b892cd4e4fd5b8
James Rice <james@purplecs.com>
2018-10-02 16:34:01 +0100

    creating an edit flow for stores

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
+  req, flash('error', 'Something Happened');
   res.render('index');
 };
 
@@ -11,9 +12,44 @@ exports.addStore = (req, res) => {
 
 exports.createStore = async (req, res) => {
   //Req = Items from the form submit
-  const store = new Store(req.body);
-  //Don't move along until store has saved
-  await store.save();
+  const store = await new Store(req.body).save();
+  //Keep user updated on client side
+  req.flash(
+    'success',
+    `Sucessfully Created ${store.name}. Care to leave a review?`
+  );
   //Redirect to homepage
-  res.redirect('/');
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  //1. Query the db for a list of the stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  //1. Find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id });
+  //res.json(store);
+  //2.Confirm they are the owner of the store
+
+  //3. Render out the edit form so user can update
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  //1. Find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, //Return the new store instead of the old store
+    runValidators: true
+  }).exec(); //Run Query
+  //2. Redirect them to the store and tell them it worked
+  req.flash(
+    'success',
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${
+      store.slug
+    }">View Store</a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
 };
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -6,8 +6,12 @@ const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here - Catch errrors from helper
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
+//passing the id as a parameter to update the store
+router.post('/add/:id', catchErrors(storeController.updateStore));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 module.exports = router;
