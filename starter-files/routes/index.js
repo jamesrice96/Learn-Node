23947afbb400be2c752ed2495b428d50b679fd82
James Rice <james@purplecs.com>
2018-10-11 17:01:55 +0100

    Creating user accounts

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 // Controllers
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 // Error handling
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -33,5 +34,13 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// 1.Validate the registration data
+// 2.Register the user
+// 3.Log them in
+router.post('/register', userController.validateRegister);
 
 module.exports = router;
